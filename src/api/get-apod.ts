2b89b4527db60cd749e586a44e44d5c00350f80f
sp <slinkin.petr@main.ru>
2020-05-08 17:42:59 +0300

    Move api types

--- src/api/get-apod.ts
+++ src/api/get-apod.ts
@@ -1,15 +1,5 @@
 import { api } from './api';
-
-export type Apod = {
-  copyright: string;
-  date: string;
-  explanation: string;
-  hdurl: string;
-  url: string;
-  mediaType: string;
-  serviceVersion: string;
-  title: string;
-};
+import { Apod } from './types';
 
 export async function getApod(): Promise<Apod> {
   const { data } = await api.get('planetary/apod');
---
+++ src/api/types.ts
@@ -0,0 +1,10 @@
+export type Apod = {
+  copyright: string;
+  date: string;
+  explanation: string;
+  hdurl: string;
+  url: string;
+  mediaType: string;
+  serviceVersion: string;
+  title: string;
+};
